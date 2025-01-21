665b6a95e2cbcf4dc3c53b870e3b12c505ccfd29
enbraining <me@bricn.net>
2025-01-21 21:26:00 +0900

    🎉 Hello discord

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import core, { debug } from '@actions/core'
+import ky from 'ky'
+
+async function main(){
+    debug("Loading input parameter.")
+
+    const webhookUrl = core.getInput("webhook-url")
+    const username = core.getInput("username")
+    const content = core.getInput("message")
+
+    debug("Send webhook message.")
+
+    const body = {
+        username: username,
+        content: content
+    }
+
+    await ky.post(webhookUrl, { json: body })
+}
+
+main()
