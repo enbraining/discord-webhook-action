ccb03aa3f678f95a4942602b9ffb2429fc6f9026
enbraining <me@bricn.net>
2025-01-23 13:14:51 +0900

    fetch try-catch

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,56 @@
-import { debug } from '@actions/core'
-import ky from 'ky'
-import { Action, Config, Embed, Type } from './types'
-import { getConfig } from './utils'
+import { debug, error, info } from "@actions/core";
+import ky from "ky";
+import { Action, Config, Embed, Type } from "./types";
+import { getConfig } from "./utils";
 
 function getEmbed(config: Config): Partial<Embed> {
-    const embed: Partial<Embed> = {
-        title: config.title,
-        description: config.description,
-        url: config.embedUrl,
-        color: config.color,
-        image: config.imageUrl ? { url: config.imageUrl } : undefined,
-    }
+  const embed: Partial<Embed> = {
+    title: config.title,
+    description: config.description,
+    url: config.embedUrl,
+    color: config.color,
+    image: config.imageUrl ? { url: config.imageUrl } : undefined,
+  };
 
-    return embed
+  return embed;
 }
 
-async function main(){
-    const config: Config = getConfig()
-    const action: Partial<Action> = {}
-    
-    if(config.username){
-        action.username = config.username
-    }
-    
-    switch(config.type){
-        case Type.CONTENT: {
-            action['content'] = config.content
-        };
-        
-        case Type.EMBEDS: {
-            const embed = getEmbed(config)
-            action['embeds'] = [embed]
-        };
+async function main() {
+  const config: Config = getConfig();
+  const action: Partial<Action> = {};
+
+  if (config.username) {
+    action.username = config.username;
+  }
+
+  switch (config.type) {
+    case Type.CONTENT: {
+      action["content"] = config.content;
     }
 
-    debug("Send webhook message.")
+    case Type.EMBED: {
+      const embed = getEmbed(config);
+      action["embeds"] = [embed];
+    }
 
-    await ky.post(config.webhookUrl, { json: action })
+    case Type.FILE: {
+      error("It's not implemented yet.");
+    }
+
+    case Type.POLL: {
+      error("It's not implemented yet.");
+    }
+  }
+
+  debug("Send webhook message.");
+
+  await ky
+    .post(config.webhookUrl, {
+      json: action,
+    })
+    .catch((reason: any) => {
+      error(reason);
+    });
 }
 
-main()
+main();
--- src/types.ts
+++ src/types.ts
@@ -1,36 +1,36 @@
 export interface Action {
-    username: string
-    content: string
-    embeds: Partial<Embed>[]
+  username: string;
+  content: string;
+  embeds: Partial<Embed>[];
 }
 
 export interface Embed {
-    title?: string
-    description?: string;
-    url?: string;
-    image?: Image
-    color?: string
+  title?: string;
+  description?: string;
+  url?: string;
+  image?: Image;
+  color?: string;
 }
 
 export interface Image {
-    url: string
+  url: string;
 }
 
 export enum Type {
-    CONTENT = "CONTENT", 
-    EMBEDS = "EMBEDS", 
-    FILE = "FILE", 
-    POLL = "POLL"
+  CONTENT = "CONTENT",
+  EMBED = "EMBED",
+  FILE = "FILE",
+  POLL = "POLL",
 }
 
 export interface Config {
-    webhookUrl: string,
-    type: string | undefined,
-    username: string | undefined,
-    content: string | undefined,
-    title: string | undefined,
-    embedUrl: string | undefined,
-    description: string | undefined,
-    color: string | undefined,
-    imageUrl: string | undefined
+  webhookUrl: string;
+  type: string | undefined;
+  username: string | undefined;
+  content: string | undefined;
+  title: string | undefined;
+  embedUrl: string | undefined;
+  description: string | undefined;
+  color: string | undefined;
+  imageUrl: string | undefined;
 }
